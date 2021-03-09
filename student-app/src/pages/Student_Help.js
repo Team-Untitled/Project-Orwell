3935787cba8e23e456883a624150fd45196305fa
Hansani <kavindi.2019598@iit.ac.lk>
2021-03-09 11:56:06 +0530

    Add front end

---
+++ student-app/src/pages/Student_Help.js
@@ -0,0 +1,36 @@
+import React,{Component} from 'react';
+
+class  Student_Help extends React.Component{
+    render(){
+        return(
+                <div>
+                   <div class="bodyhelp">
+                  <div class="Help">
+                   <div class="container3">
+
+                   <h1>Help</h1>
+     <h6>User guide to use Project-Orwell</h6> 
+     <hr width='30%'/>
+     <h3>Overview of the system</h3> 
+     <p> “Project-Orwell”- A System for Monitoring Student’s Attention.Project-Orwell which will be a system for monitoring students’
+	 attention in a classroom and updating the teacher or the lecturer in real time. A system contents with an application connected with web 
+	 cameras. By using face recognition, the application shows the students in the classroom with their identities. Moreover, applying a machine
+	 learning module for a combined approach of face detection and pose estimation, the system highlighted the students who are in lack attention.
+	 A system contents with an application connected with web cameras. By using face recognition, the application shows the students in the classroom 
+	 with their identities. Moreover, applying a machine learning module for a combined approach of face detection and pose estimation, the system 
+	 highlighted the students who are in lack attention.Finally a report is generated with the details about attention level and attendance of every
+	 student individually.</p>
+     <h3>How the system works</h3> 
+   <p> The main home page of student has 2 options-Join Session and View Profile.Join Session section allows student to log into the student.
+   View Profile navigates the students to view their own profiles</p>
+   <p>Please refer the special user manual documentation provided for more information</p>
+
+</div>
+                       
+                   </div>
+                </div>
+                </div>
+        )
+    }
+}
+export default  Student_Help
